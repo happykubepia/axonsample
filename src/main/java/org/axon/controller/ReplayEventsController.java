f11c6247f958be801d57a4167a1238e94d7f5275
이해경 <ondal@ihaegyeong-ui-MacBookAir.local>
2024-01-18 03:15:12 +0900

    add replay api

--- src/main/java/org/axon/controller/ReplayEventsController.java
+++ src/main/java/org/axon/controller/ReplayEventsController.java
@@ -35,7 +35,7 @@ class ReplayEventsController {
     @Operation(summary = "지정한 Processing Group의 Event Handler를 지정한 시간 이후 Event만 Replay하면서 수행함")
     @Parameters({
             @Parameter(name = "processingGroup", in= ParameterIn.PATH, description = "Processing Groupname",
-                    required = true, allowEmptyValue = false, example="orders"),
+                    required = true, allowEmptyValue = false, example="elephant"),
             @Parameter(name = "startDateTime", in= ParameterIn.PATH, description = "시작일시(생략 시 모든 Event Replay)",
                     required = false, allowEmptyValue = true, example = "2024-01-01T00:00:00.00Z")
     })
