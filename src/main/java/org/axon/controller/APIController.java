c1d0003430178dee06cabed6e68a3ca360041789
이해경 <ondal@ihaegyeong-ui-MacBookAir.local>
2024-01-18 12:59:17 +0900

    add service layer

--- src/main/java/org/axon/controller/APIController.java
+++ src/main/java/org/axon/controller/APIController.java
@@ -6,123 +6,58 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.axon.command.CreateElephantCommand;
-import org.axon.command.EnterElephantCommand;
-import org.axon.command.ExitElephantCommand;
 import org.axon.dto.ElephantDTO;
-import org.axon.dto.StatusEnum;
 import org.axon.entity.Elephant;
-import org.axon.queries.GetElephantQuery;
-import org.axon.repository.ElephantRepository;
+import org.axon.service.ElephantService;
 import org.axon.vo.ResultVO;
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.messaging.responsetypes.ResponseTypes;
-import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 @Tag(name = "Order service API", description="Order service API" )
 @Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class APIController {
+
+    private final ElephantService elephantService;
+
     @Autowired
-    private transient CommandGateway commandGateway;
-    @Autowired
-    private transient QueryGateway queryGateway;
-    @Autowired
-    private ElephantRepository elephantRepository;
+    public APIController(ElephantService elephantService) {
+        this.elephantService = elephantService;
+    }
 
     @PostMapping("/create")
     @Operation(summary = "코끼리 생성 API")
     private ResultVO<CreateElephantCommand> create(@RequestBody ElephantDTO elephant) {
         log.info("[@PostMapping '/create'] Executing create: {}", elephant.toString());
 
-        ResultVO<CreateElephantCommand> retVo = new ResultVO<>();
-        CreateElephantCommand cmd = CreateElephantCommand.builder()
-                .id(RandomStringUtils.random(3, false, true))
-                .name(elephant.getName())
-                .weight(elephant.getWeight())
-                .status(StatusEnum.READY.value())
-                .build();
-
-        try {
-            commandGateway.sendAndWait(cmd, 30, TimeUnit.SECONDS);
-            retVo.setReturnCode(true);
-            retVo.setReturnMessage("Success to create elephant");
-            retVo.setResult(cmd);
-        } catch(Exception e) {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage(e.getMessage());
-        }
-
-        return retVo;
+        return elephantService.create(elephant);
     }
 
 
     @PostMapping("/enter/{id}")
     @Operation(summary = "냉장고에 넣기 API")
     @Parameters({
-            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true, allowEmptyValue = false)
+            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true)
     })
     private ResultVO<String> enter(@PathVariable(name = "id") String id) {
         log.info("[@PostMapping '/enter'] Id: {}", id);
-        ResultVO<String> retVo = new ResultVO<>();
 
-        //-- check validation
-        Optional<Elephant> optElephant = elephantRepository.findById(id);
-        Elephant elephant = optElephant.isPresent() ? optElephant.get() : null;
-        if(!(elephant.getStatus().equals(StatusEnum.READY.value()) ||
-                elephant.getStatus().equals(StatusEnum.EXIT.value()))) {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage("현재 코끼리 상태는 " + elephant.getStatus()+ ". 코끼리 상태가 'Ready' 또는 'Exit'인 경우만 넣을 수 있습니다.");
-            return retVo;
-        }
-        //--send command
-        try {
-            commandGateway.sendAndWait(EnterElephantCommand.builder().id(id).build(), 30, TimeUnit.SECONDS);
-            retVo.setReturnCode(true);
-            retVo.setReturnMessage("Success to request enter elephant");
-        } catch(Exception e) {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage(e.getMessage());
-        }
-        return retVo;
+        return elephantService.enter(id);
     }
 
     @PostMapping("/exit/{id}")
     @Operation(summary = "냉장고에서 꺼내기 API")
     @Parameters({
-            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true, allowEmptyValue = false)
+            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true)
     })
     private ResultVO<String> exit(@PathVariable(name = "id") String id) {
         log.info("[@PostMapping '/exit'] Id: {}", id);
-        ResultVO<String> retVo = new ResultVO<>();
 
-        //-- check validation
-        Optional<Elephant> optElephant = elephantRepository.findById(id);
-        Elephant elephant = optElephant.isPresent() ? optElephant.get() : null;
-        if(!elephant.getStatus().equals(StatusEnum.ENTER.value())) {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage("현재 코끼리 상태는 " + elephant.getStatus()+ ". 코끼리 상태가 'Enter'인 경우만 꺼낼 수 있습니다.");
-            return retVo;
-        }
-
-        //-- send command
-        try {
-            commandGateway.sendAndWait(ExitElephantCommand.builder().id(id).build(), 30, TimeUnit.SECONDS);
-            retVo.setReturnCode(true);
-            retVo.setReturnMessage("Success to request exit elephant");
-        } catch(Exception e) {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage(e.getMessage());
-        }
-        return retVo;
+        return elephantService.exit(id);
     }
 
     @GetMapping("/elephants")
@@ -130,62 +65,28 @@ public class APIController {
     private ResultVO<List<Elephant>> getLists() {
         log.info("[@GetMapping '/elephants']");
 
-        ResultVO<List<Elephant>> retVo = new ResultVO<>();
-        List<Elephant> elephants = queryGateway.query("list", "",
-                ResponseTypes.multipleInstancesOf(Elephant.class)).join();
-        if(elephants != null) {
-            retVo.setReturnCode(true);
-            retVo.setReturnMessage("코끼리수: "+elephants.size());
-            retVo.setResult(elephants);
-        } else {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage("No registered elephant");
-        }
-        return retVo;
+        return elephantService.getLists();
     }
 
     @GetMapping("/elephant/{id}")
     @Operation(summary = "코끼리 정보 API")
     @Parameters({
-            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true, allowEmptyValue = false)
+            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true)
     })
     private ResultVO<Elephant> getElephant(@PathVariable(name = "id") String id) {
         log.info("[@GetMapping '/elephant/{id}'] Id: {}", id);
 
-        ResultVO<Elephant> retVo = new ResultVO<>();
-        Elephant elephant = queryGateway.query(new GetElephantQuery(id),
-                ResponseTypes.instanceOf(Elephant.class)).join();
-        if(elephant != null) {
-            retVo.setReturnCode(true);
-            retVo.setReturnMessage("ID: "+ id);
-            retVo.setResult(elephant);
-        } else {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage("Can't get elephant for Id:"+id);
-        }
-        return retVo;
+        return elephantService.getElephant(id);
     }
 
     @DeleteMapping("/elephant/{id}")
     @Operation(summary = "코끼리 삭제 API")
     @Parameters({
-            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true, allowEmptyValue = false)
+            @Parameter(name = "id", in= ParameterIn.PATH, description = "코끼리ID", required = true)
     })
     private ResultVO<Elephant> deleteElephant(@PathVariable(name = "id") String id) {
         log.info("[@DeleteMapping '/elephant/{id}'] Id: {}", id);
 
-        ResultVO<Elephant> retVo = new ResultVO<>();
-        Optional<Elephant> optElephant = elephantRepository.findById(id);
-        Elephant elephant = optElephant.isPresent() ? optElephant.get() : null;
-        if(elephant != null) {
-            elephantRepository.delete(elephant);
-            retVo.setReturnCode(true);
-            retVo.setReturnMessage("Elephant ID: "+ id + " is DELETED!");
-            retVo.setResult(elephant);
-        } else {
-            retVo.setReturnCode(false);
-            retVo.setReturnMessage("Can't get elephant for Id:"+id);
-        }
-        return retVo;
+        return elephantService.deleteElephant(id);
     }
 }
---
+++ src/main/java/org/axon/service/ElephantService.java
@@ -0,0 +1,167 @@
+package org.axon.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.axon.command.CreateElephantCommand;
+import org.axon.command.EnterElephantCommand;
+import org.axon.command.ExitElephantCommand;
+import org.axon.dto.ElephantDTO;
+import org.axon.dto.StatusEnum;
+import org.axon.entity.Elephant;
+import org.axon.queries.GetElephantQuery;
+import org.axon.repository.ElephantRepository;
+import org.axon.vo.ResultVO;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class ElephantService {
+    @Autowired
+    private transient CommandGateway commandGateway;
+    @Autowired
+    private transient QueryGateway queryGateway;
+
+    private final ElephantRepository elephantRepository;
+
+    @Autowired
+    public ElephantService(ElephantRepository elephantRepository) {
+        this.elephantRepository = elephantRepository;
+    }
+
+    public ResultVO<CreateElephantCommand> create(ElephantDTO elephant) {
+        log.info("[ElephantService] Executing create: {}", elephant.toString());
+
+        ResultVO<CreateElephantCommand> retVo = new ResultVO<>();
+        CreateElephantCommand cmd = CreateElephantCommand.builder()
+                .id(RandomStringUtils.random(3, false, true))
+                .name(elephant.getName())
+                .weight(elephant.getWeight())
+                .status(StatusEnum.READY.value())
+                .build();
+
+        try {
+            commandGateway.sendAndWait(cmd, 30, TimeUnit.SECONDS);
+            retVo.setReturnCode(true);
+            retVo.setReturnMessage("Success to create elephant");
+            retVo.setResult(cmd);
+        } catch(Exception e) {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage(e.getMessage());
+        }
+
+        return retVo;
+    }
+
+    public ResultVO<String> enter(String id) {
+        log.info("[ElephantService] Executing enter for Id: {}", id);
+        ResultVO<String> retVo = new ResultVO<>();
+
+        //-- check validation
+        Elephant elephant = getEntity(id);
+        if(!(elephant.getStatus().equals(StatusEnum.READY.value()) ||
+                elephant.getStatus().equals(StatusEnum.EXIT.value()))) {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage("현재 코끼리 상태는 " + elephant.getStatus()+ ". 코끼리 상태가 'Ready' 또는 'Exit'인 경우만 넣을 수 있습니다.");
+            return retVo;
+        }
+        //--send command
+        try {
+            commandGateway.sendAndWait(EnterElephantCommand.builder().id(id).build(), 30, TimeUnit.SECONDS);
+            retVo.setReturnCode(true);
+            retVo.setReturnMessage("Success to request enter elephant");
+        } catch(Exception e) {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage(e.getMessage());
+        }
+        return retVo;
+    }
+
+
+    public ResultVO<String> exit(String id) {
+        log.info("[ElephantService] Executing exit for Id: {}", id);
+        ResultVO<String> retVo = new ResultVO<>();
+
+        //-- check validation
+        Elephant elephant = getEntity(id);
+        if(!elephant.getStatus().equals(StatusEnum.ENTER.value())) {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage("현재 코끼리 상태는 " + elephant.getStatus()+ ". 코끼리 상태가 'Enter'인 경우만 꺼낼 수 있습니다.");
+            return retVo;
+        }
+
+        //-- send command
+        try {
+            commandGateway.sendAndWait(ExitElephantCommand.builder().id(id).build(), 30, TimeUnit.SECONDS);
+            retVo.setReturnCode(true);
+            retVo.setReturnMessage("Success to request exit elephant");
+        } catch(Exception e) {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage(e.getMessage());
+        }
+        return retVo;
+    }
+
+    public ResultVO<List<Elephant>> getLists() {
+        log.info("[ElephantService] Executing getLists");
+
+        ResultVO<List<Elephant>> retVo = new ResultVO<>();
+        List<Elephant> elephants = queryGateway.query("list", "",
+                ResponseTypes.multipleInstancesOf(Elephant.class)).join();
+        if(elephants != null) {
+            retVo.setReturnCode(true);
+            retVo.setReturnMessage("코끼리수: "+elephants.size());
+            retVo.setResult(elephants);
+        } else {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage("No registered elephant");
+        }
+        return retVo;
+    }
+
+    public ResultVO<Elephant> getElephant(String id) {
+        log.info("[ElephantService] Executing getElephant for Id: {}", id);
+
+        ResultVO<Elephant> retVo = new ResultVO<>();
+        Elephant elephant = queryGateway.query(new GetElephantQuery(id),
+                ResponseTypes.instanceOf(Elephant.class)).join();
+        if(elephant != null) {
+            retVo.setReturnCode(true);
+            retVo.setReturnMessage("ID: "+ id);
+            retVo.setResult(elephant);
+        } else {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage("Can't get elephant for Id:"+id);
+        }
+        return retVo;
+    }
+
+    public ResultVO<Elephant> deleteElephant(String id) {
+        log.info("[@DeleteMapping '/elephant/{id}'] Id: {}", id);
+
+        ResultVO<Elephant> retVo = new ResultVO<>();
+        Elephant elephant = getEntity(id);
+        if(elephant != null) {
+            elephantRepository.delete(elephant);
+            retVo.setReturnCode(true);
+            retVo.setReturnMessage("Elephant ID: "+ id + " is DELETED!");
+            retVo.setResult(elephant);
+        } else {
+            retVo.setReturnCode(false);
+            retVo.setReturnMessage("Can't get elephant for Id:"+id);
+        }
+        return retVo;
+    }
+
+    private Elephant getEntity(String id) {
+        Optional<Elephant> optElephant = elephantRepository.findById(id);
+        return optElephant.orElse(null);
+    }
+}
